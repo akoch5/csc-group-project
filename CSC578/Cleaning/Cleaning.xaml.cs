bf6b60c44470420959df5300a4411de0b3d4c6db
Elise Novorolsky <enovoro2@uis.edu>
2017-12-10 17:45:00 -0600

    cleaning updates

--- CSC578/Cleaning/Cleaning.xaml.cs
+++ CSC578/Cleaning/Cleaning.xaml.cs
@@ -15,29 +15,30 @@ using System.Data;
 using System.Data.OleDb;
 using System.Collections;
 using System.Configuration;
+using System.Windows.Forms;
 
 namespace CSC578.Cleaning
 {
     /// <summary>
     /// Interaction logic for Cleaning.xaml
     /// </summary>
-    public partial class Cleaning : UserControl
+    public partial class Cleaning : System.Windows.Controls.UserControl
     {
         public class CleaningObject
         {
             private String taskName;
             private String assignee;
             private String frequency;
-            private String startDate;
-            private String endDate;
+            private DateTime startDate;
+            private DateTime endDate;
 
-            public CleaningObject(String taskName, String assignee, String frequency, String startDate, String endDate)
+            public CleaningObject(String taskName, String assignee, String frequency, DateTime startDate, DateTime endDate)
             {
-                taskName = this.taskName;
-                assignee = this.assignee;
-                frequency = this.frequency;
-                startDate = this.startDate;
-                endDate = this.endDate;                
+                this.taskName = taskName;
+                this.assignee = assignee;
+                this.frequency = frequency;
+                this.startDate = startDate;
+                this.endDate = endDate;                
             }
 
             public String getFrequency()
@@ -52,14 +53,28 @@ namespace CSC578.Cleaning
             {
                 return this.assignee;
             }
+            public DateTime getStartDate()
+            {
+                return this.startDate;
+            }
+            public DateTime getEndDate()
+            {
+                return this.endDate;
+            }
         }
 
+
         public static List<CleaningObject> cleaningList;
+
 
         public Cleaning()
         {
             InitializeComponent();
             cleaningList = getCleaningList();
+            displayDate.Text = DateTime.Today.ToString();
+            populateCleaningGrid();
+            populateAlerts();
+
         }
 
        
@@ -86,12 +101,21 @@ namespace CSC578.Cleaning
             if (MainWindow.con.State != ConnectionState.Open)
                 MainWindow.con.Open();
             cmd.Connection = MainWindow.con;
-            cmd.CommandText = "select * from ";
+            cmd.CommandText = "select * from cleaningData where CurrentDate = DateValue (' " + Convert.ToDateTime(displayDate.Text) + "')";
             OleDbDataAdapter da = new OleDbDataAdapter(cmd);
             MainWindow.dt = new DataTable();
             da.Fill(MainWindow.dt);
-            //cleaningGrid.ItemsSource = dt.AsDataView();
+            cleaningGrid.ItemsSource = MainWindow.dt.AsDataView();
             MainWindow.con.Close();
+
+            if (cleaningGrid.Columns.Count > 0)
+            {
+                cleaningGrid.Columns[0].IsReadOnly = true;
+                cleaningGrid.Columns[0].Visibility = System.Windows.Visibility.Hidden;
+                cleaningGrid.Columns[1].IsReadOnly = true;
+                cleaningGrid.Columns[4].IsReadOnly = true;
+                cleaningGrid.Columns[5].IsReadOnly = true;
+            }
         }
 
         private static List<CleaningObject> getCleaningList()
@@ -109,39 +133,124 @@ namespace CSC578.Cleaning
             OleDbDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                cleaningList.Add(new CleaningObject(rdr["taskName"].ToString(), rdr["assignee"].ToString(), rdr["frequency"].ToString(), rdr["startDate"].ToString(), rdr["endDate"].ToString()));
+               cleaningList.Add(new CleaningObject(rdr.GetString(1), rdr.GetString(2), rdr.GetString(3), rdr.GetDateTime(4), rdr.GetDateTime(5)));
             }
             rdr.Close();
             MainWindow.con.Close();
             return cleaningList;
         }
 
-        public static void addCleaningData()
+        public static void addCleaningData(CleaningObject cleaningTask)
         {
-            DateTime currentDate = DateTime.Today;
-            foreach (CleaningObject c in cleaningList)
+            
+            DateTime currentDate = cleaningTask.getStartDate();
+
+            while (cleaningTask.getEndDate().CompareTo(currentDate) >= 0)
             {
-                switch (c.getFrequency())
+                switch (cleaningTask.getFrequency())
                 {
                     case "Daily":
+                        MainWindow.editDB("insert into cleaningData (Task, Assigned, Completed, CurrentDate) " +
+                                "values " + $"('{cleaningTask.getTaskName()}', '{cleaningTask.getAssignee()}', {false}, '{currentDate}')");
                         break;
                     case "Weekly":
+                        if ((currentDate.DayOfYear- cleaningTask.getStartDate().DayOfYear)%7 == 0)
+                        {
+                            MainWindow.editDB("insert into cleaningData (Task, Assigned, Completed, CurrentDate) " +
+                                "values " + $"('{cleaningTask.getTaskName()}', '{cleaningTask.getAssignee()}', {false}, '{currentDate}')");
+                        }
                         break;
                     case "Bi-Weekly":
+                        if ((currentDate.DayOfYear - cleaningTask.getStartDate().DayOfYear) % 14 == 0)
+                        {
+                            MainWindow.editDB("insert into cleaningData (Task, Assigned, Completed, CurrentDate) " +
+                                "values " + $"('{cleaningTask.getTaskName()}', '{cleaningTask.getAssignee()}', {false}, '{currentDate}')");
+                        }
                         break;
                     case "Monthly":
+                        if (currentDate.Day == cleaningTask.getStartDate().Day)
+                        {
+                            MainWindow.editDB("insert into cleaningData (Task, Assigned, Completed, CurrentDate) " +
+                                "values " + $"('{cleaningTask.getTaskName()}', '{cleaningTask.getAssignee()}', {false}, '{currentDate}')");
+                        }
                         break;
                     case "One-Time":
+                        if (currentDate == cleaningTask.getStartDate())
+                        {
+                            MainWindow.editDB("insert into cleaningData (Task, Assigned, Completed, CurrentDate) " +
+                                "values " + $"('{cleaningTask.getTaskName()}', '{cleaningTask.getAssignee()}', {false}, '{currentDate}')");
+                        }
                         break;
                     default:
                         break;
                 }
-               
-                MainWindow.editDB("insert into cleaningData (Task, Assigned, Completed, CurrentDate) " +
-                    "values " + $"('{c.getTaskName()}', '{c.getAssignee()}', {false}, {11/18/17})");
-
+                currentDate = currentDate.AddDays(1);
             }
            
+        }
+
+
+        private void rightButton_Click(object sender, RoutedEventArgs e)
+        {
+            displayDate.Text = Convert.ToDateTime(displayDate.Text).AddDays(1).ToString();
+            populateCleaningGrid();
+        }
+
+        private void leftButton_Click(object sender, RoutedEventArgs e)
+        {
+            displayDate.Text = Convert.ToDateTime(displayDate.Text).AddDays(-1).ToString();
+            populateCleaningGrid();
+        }
+
+
+        private void cleaningGrid_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (cleaningGrid.CurrentCell.Column != null)
+            {
+                if (cleaningGrid.CurrentCell.Column.Header.ToString() == "Completed")
+                {
+                    DataRowView dataRow = (DataRowView)cleaningGrid.CurrentCell.Item;
+                    if (dataRow.Row.ItemArray[3].Equals(false))
+                    {
+                
+                        MainWindow.editDB("Update cleaningData set completed = true, CompletedTime = '" + DateTime.Now + "' where ID = " + dataRow.Row.ItemArray[0]);
+                        populateCleaningGrid();
+                    }
+                    else
+                    {
+                        MainWindow.editDB("Update cleaningData set completed = false, CompletedTime = null where ID = " + dataRow.Row.ItemArray[0]);
+                        populateCleaningGrid();
+                    }
+                                
+                }
+
+   
+            }
+
+        }
+
+        private void populateAlerts()
+        {
+            MainWindow.con = new OleDbConnection();
+            MainWindow.con.ConnectionString = ConfigurationManager.
+                        ConnectionStrings["CSC578.Properties.Settings.DatabaseConnectionString"].
+                            ToString();
+            OleDbCommand cmd = new OleDbCommand();
+            if (MainWindow.con.State != ConnectionState.Open)
+                MainWindow.con.Open();
+            cmd.Connection = MainWindow.con;
+            cmd.CommandText = "select * from cleaningData where CurrentDate < DateValue ('" + DateTime.Now + "') and Completed = false" ;
+            OleDbDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                System.Windows.Controls.Label newLabel = new System.Windows.Controls.Label();
+                newLabel.Content = "Task Overdue: " + rdr.GetString(1) + " Due: " + rdr.GetDateTime(5).ToString();
+                alerts.Items.Add(newLabel);
+            }
+            rdr.Close();
+            MainWindow.con.Close();
+
+            
         }
 
     }
